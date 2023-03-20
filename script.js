3ae303acb7f8ef786ac1e618b4c4aab37a141e4e
Evantopian <EvanAcademics@gmail.com>
2023-03-19 21:06:20 -0400

    added add row and col

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-// Declare global variables
 let numRows = 0;
 let numCols = 0;
-let colorSelected; 
+const table = document.getElementById("grid");
 
-// Add a row
-function addR() {
-    alert("Clicked Add Row"); // Replace this line with your code.
+function addRow() {
+  if (numCols === 0) {
+    numCols++;
+  }
+  numRows++;
+  const row = document.createElement("tr");
+  for (let i = 0; i < numCols; i++) {
+    const cell = document.createElement("td");
+    cell.onclick = function () {
+      cell.style.background = colorSelected;
+    };
+    row.appendChild(cell);
+  }
+  table.appendChild(row);
 }
 
-// Add a column
-function addC() {
-    alert("Clicked Add Col"); // Replace this line with your code.
+function addColumn() {
+  if (numRows === 0) {
+    numRows++;
+  }
+  numCols++;
+  for (let i = 0; i < numRows; i++) {
+    const cell = document.createElement("td");
+    cell.onclick = function () {
+      cell.style.background = colorSelected;
+    };
+    table.rows[i].appendChild(cell);
+  }
 }
 
 // Remove a row
